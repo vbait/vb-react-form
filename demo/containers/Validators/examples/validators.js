68f3ddabe172f8b961827157001d0bd6abf718d7
Vitalii Baitaliuk <vitalii@cog1.com>
2017-04-07 12:23:54 +0300

    create demo for validators

--- demo/containers/Validators/examples/validators.js
+++ demo/containers/Validators/examples/validators.js
@@ -7,7 +7,7 @@ class Example extends React.Component {
     const phoneValidator = new PhoneValidator('en-US');
     const siteValidator = new WebSiteValidator();
     const currencyValidator = new CurrencyValidator();
-    const passwordValidator = new PasswordValidator();
+    const passwordValidator = new PasswordValidator(false); 
     const minLengthValidator = new MinLengthValidator(5);
     const maxLengthValidator = new MaxLengthValidator(10);
     const minValueValidator = new MinValueValidator(5);
@@ -55,6 +55,83 @@ class Example extends React.Component {
             </li>
             <li>
               <b>invalid value: </b> {phoneValidator.isInvalid('120255501744') && phoneValidator.error()}
+            </li>
+          </ul>
+        </div>
+        <div>
+          new WebSiteValidator()
+          <ul>
+            <li>
+              <b>valid value: </b> {siteValidator.isValid('google.com') && 'Yes'}
+            </li>
+            <li>
+              <b>invalid value: </b> {siteValidator.isInvalid('google') && siteValidator.error()}
+            </li>
+          </ul>
+        </div>
+        <div>
+          new CurrencyValidator()
+          <ul>
+            <li>
+              <b>valid value: </b> {currencyValidator.isValid('12.45') && 'Yes'}
+            </li>
+            <li>
+              <b>invalid value: </b> {currencyValidator.isInvalid('12.456') && currencyValidator.error()}
+            </li>
+          </ul>
+        </div>
+        <div>
+          new PasswordValidator()
+          <ul>
+            <li>
+              <b>valid value: </b> {passwordValidator.isValid('123456a') && 'Yes'}
+            </li>
+            <li>
+              <b>invalid value: </b> {passwordValidator.isInvalid('123456') && passwordValidator.error()}
+            </li>
+          </ul>
+        </div>
+        <div>
+          new MinLengthValidator(5)
+          <ul>
+            <li>
+              <b>valid value: </b> {minLengthValidator.isValid('12345') && 'Yes'}
+            </li>
+            <li>
+              <b>invalid value: </b> {minLengthValidator.isInvalid('1234') && minLengthValidator.error()}
+            </li>
+          </ul>
+        </div>
+        <div>
+          new MaxLengthValidator(10)
+          <ul>
+            <li>
+              <b>valid value: </b> {maxLengthValidator.isValid('1234567890') && 'Yes'}
+            </li>
+            <li>
+              <b>invalid value: </b> {maxLengthValidator.isInvalid('12345678901') && maxLengthValidator.error()}
+            </li>
+          </ul>
+        </div>
+        <div>
+          new MinValueValidator(5)
+          <ul>
+            <li>
+              <b>valid value: </b> {minValueValidator.isValid(5) && 'Yes'}
+            </li>
+            <li>
+              <b>invalid value: </b> {minValueValidator.isInvalid(4.99) && minValueValidator.error()}
+            </li>
+          </ul>
+        </div>
+        <div>
+          new MaxLengthValidator(5)
+          <ul>
+            <li>
+              <b>valid value: </b> {maxValueValidator.isValid(10) && 'Yes'}
+            </li>
+            <li>
+              <b>invalid value: </b> {maxValueValidator.isInvalid(10.1) && maxValueValidator.error()}
             </li>
           </ul>
         </div>
