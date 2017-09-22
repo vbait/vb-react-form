06ea0c1966d62bf6a19ea603f2779c2be35bcd38
Vitalii Baitaliuk <flampschelsea@gmail.com>
2017-09-22 17:56:43 +0300

    new forms

--- demo/containers/FormList/Fields.js
+++ demo/containers/FormList/Fields.js
@@ -96,13 +96,13 @@ class ColorField extends React.PureComponent {
           position: 'absolute',
           zIndex: '2',
         },
-        cover: {
-          position: 'fixed',
-          top: '0px',
-          right: '0px',
-          bottom: '0px',
-          left: '0px',
-        },
+        // cover: {
+        //   position: 'fixed',
+        //   top: '0px',
+        //   right: '0px',
+        //   bottom: '0px',
+        //   left: '0px',
+        // },
       },
     });
 
